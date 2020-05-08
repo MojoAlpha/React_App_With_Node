71793307bdaa5fed8511d310c019f682086cea91
MojoAlpha <mohitp@mnnit.ac.in>
2020-05-08 15:15:57 +0530

    just there

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use('/api',orderRoutes);
 app.use('/api',stripeRoutes); 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/./projfrontend/public/index.html'))
+    res.sendFile(path.join(__dirname + './projfrontend/public/index.html'))
   })
 
 
